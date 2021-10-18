8f0c0f08d55bc49ed971ab90e9de385e22f4f834
error404cyber <67262189+error404cyber@users.noreply.github.com>
2021-10-18 20:48:16 +0300

    general code

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 int main()
 {
     double a = 0, b = 0, c = 0, x1 = 0, x2 = 0, d = 0;
-    KORNI kor=0;
+    KORNI kor=LIN_ZERO;
     scan_coefficients(&a, &b, &c);
     kor = resh(a, b, c, &x1, &x2);
     answ(x1, x2, kor);
