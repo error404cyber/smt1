0dc8fba926f53c35a070af03f2fc94a76142ab7e
error404cyber <67262189+error404cyber@users.noreply.github.com>
2021-10-18 21:11:47 +0300

    general code

--- main.c
+++ main.c
@@ -8,7 +8,6 @@ int main()
 {
     double a = 0, b = 0, c = 0, x1 = 0, x2 = 0, d = 0;
     KORNI kor=LIN_ZERO;
-    scan_coefficients(&a, &b, &c);
     run_all_tests();
     printf("Ready\n");
     return 0;
