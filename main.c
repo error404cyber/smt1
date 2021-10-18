4d2e14d747309fedfd10fc5e649872ea0c583e09
error404cyber <67262189+error404cyber@users.noreply.github.com>
2021-10-18 20:34:27 +0300

    main decision

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <assert.h>
+#include <math.h>
+#include "Quadreq.c"
+
+int main()
+{
+    double a = 0, b = 0, c = 0, x1 = 0, x2 = 0, d = 0;
+    KORNI kor;
+    scan_coefficients(&a, &b, &c);
+    kor = resh(a, b, c, &x1, &x2);
+    answ(x1, x2, kor);
+    return 0;
+}
