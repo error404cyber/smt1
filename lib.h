aa394fc968cd7d5a9e603c9978c1bc025a6338b1
error404cyber <67262189+error404cyber@users.noreply.github.com>
2021-10-18 10:27:44 +0300

    Delete lib.h

--- lib.h
+++
@@ -1,7 +0,0 @@
-int srav(double x, double y);
-void scan_coefficients(double *ptr_to_a, double *ptr_to_b, double *ptr_to_c);
-void discr(double a, double b, double c, double *D);
-void resh_kv(double a, double b, double c, double d, double *x1, double *x2);
-KORNI resh_lin(double b, double c, double *x1);
-KORNI resh(double a, double b, double c, double *x1, double *x2);
-void answ(double x1, double x2, KORNI kor);
