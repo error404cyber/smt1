5be1480b2392448ab9f25d829a4606f79cbf3845
error404cyber <67262189+error404cyber@users.noreply.github.com>
2021-10-18 20:43:09 +0300

    Update lib.h

--- lib.h
+++ lib.h
@@ -10,7 +10,7 @@ typedef enum
     LIN_INF
 } KORNI;
 
-int srav(double x, double y);
+int isEqual(double x, double y);
 void scan_coefficients(double *ptr_to_a, double *ptr_to_b, double *ptr_to_c);
 void discr(double a, double b, double c, double *D);
 void resh_kv(double a, double b, double c, double d, double *x1, double *x2);
