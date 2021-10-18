ae6bdfd2ab7ecf01b1b77ee39705f340606d9b01
error404cyber <67262189+error404cyber@users.noreply.github.com>
2021-10-18 21:10:02 +0300

    general code

--- main.c
+++ main.c
@@ -2,13 +2,14 @@
 #include <assert.h>
 #include <math.h>
 #include "Quadreq.c"
+#include "lib_testQuadrEq.h"
 
 int main()
 {
     double a = 0, b = 0, c = 0, x1 = 0, x2 = 0, d = 0;
     KORNI kor=LIN_ZERO;
     scan_coefficients(&a, &b, &c);
-    kor = resh(a, b, c, &x1, &x2);
-    answ(x1, x2, kor);
+    run_all_tests();
+    printf("Ready");
     return 0;
 }
